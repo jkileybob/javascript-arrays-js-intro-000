e9055217fdde9c2ae2e81720411f3ffa01a07261
Kiley Bobbitt <jkileybob@aol.com>
2018-07-17 20:46:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1)
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length, -1)
 }
